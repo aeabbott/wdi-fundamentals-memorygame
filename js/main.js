e359bc149846094062dff739ecb3100318c43a3c
Amanda Abbott <amandaabbott@Amandas-MBP.home>
2017-03-08 15:44:31 -0500

    created game logic

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+var card0ne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king"; 
+
+console.log("User flipped" + " " + card0ne);
+console.log("User flipped" + " " + cardThree);
